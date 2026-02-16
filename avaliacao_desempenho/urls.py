0159afc3b9d50317e40a2a8317d6d22c8c0f197a
Gustavo Silva <gustavo.silva@nees.ufal.br>
2026-02-16 16:13:46 -0300

    :books: docs: adiciona e implementa a depencia do swagger para a documentacao dos endpoints

--- avaliacao_desempenho/urls.py
+++ avaliacao_desempenho/urls.py
@@ -18,10 +18,34 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AvaliaçãoDesempenho API",
+        default_version="v1",
+        description="API para gerenciamento de avaliações de desempenho",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="gass@ic.ufal.br"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/", include("avaliacao.urls")),
+    path("api/v1/", include("colaborador.urls")),
 ]
 
 if settings.DEBUG:
