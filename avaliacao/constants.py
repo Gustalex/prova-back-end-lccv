fb61c4dbd290fd5e4eafd365fa0f7b39ae3aa8d5
Gustavo Silva <gustavo.silva@nees.ufal.br>
2026-02-17 12:01:33 -0300

    :recycle: refactor:implementa os arquivos de constants e mixins para reduzir a repeticao de codigo durante a validacao de processos via status

---
+++ avaliacao/constants.py
@@ -0,0 +1,11 @@
+MSG_AVALIACAO_INICIADA = "Avaliação iniciada com sucesso"
+MSG_FEEDBACK_REGISTRADO = "Feedback registrado com sucesso"
+MSG_AVALIACAO_CONCLUIDA = "Avaliação concluída com sucesso"
+MSG_NOTA_ATUALIZADA = "Nota atualizada com sucesso"
+
+MSG_ERRO_STATUS_INVALIDO_ATUALIZACAO = (
+    "Não é possível atualizar uma avaliação com status '{status}'."
+)
+MSG_ERRO_STATUS_INVALIDO_ITEM = (
+    "Não é possível atualizar itens de uma avaliação com status '{status}'."
+)
